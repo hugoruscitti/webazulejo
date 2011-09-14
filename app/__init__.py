d9c5f07c02c4931e08b0e6d0de7bf4fc6655a966
Hugo Ruscitti <hugoruscitti@gmail.com>
2011-09-14 16:57:41 -0300

    simplificando la aplicacion flask para que simplemente muestre un template mas lindo...

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 from app.db import session_remove
-from app.controller.project import project
 
 app = Flask(__name__)
-app.config.from_pyfile('settings.py')
-app.register_module(project)
+
+@app.route('/')
+def index():
+    return render_template('index.html', model=[])
+
 
 @app.after_request
 def shutdown_session(response):
--- app/controller/project.py
+++
@@ -1,8 +0,0 @@
-from flask import Module, render_template
-
-project = Module(__name__)
-
-@project.route('/')
-def index():
-    return render_template('project.html', model=[])
-
