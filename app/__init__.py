ad739d2fd91cb00024e2a4a80e7137c4c0c2d46e
Hugo Ruscitti <hugoruscitti@mojito.(none)>
2011-09-15 11:29:58 -0300

    quitando dependencias a la base de datos.

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,7 @@
 from flask import Flask, render_template
-from app.db import session_remove
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html', model=[])
-
-
-@app.after_request
-def shutdown_session(response):
-    session_remove()
-    return response
-
